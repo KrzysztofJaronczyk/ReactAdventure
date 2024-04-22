7be38b49ad437333f7d80da3f975c407ab9ac620
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2024-04-22 15:31:47 +0200

    Add a secion using components

--- 01_FirstProject/src/App.jsx
+++ 01_FirstProject/src/App.jsx
@@ -1,10 +1,40 @@
+import componentsImg from './assets/components.png'
+import { CORE_CONCEPTS } from './data.js'
+
+const readctDescriptions = ['Fundamental', 'Cruical', 'Important', 'Essential']
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * max)
+}
+
 function Header() {
+	const description = readctDescriptions[getRandomInt(readctDescriptions.length)]
 	return (
 		<header>
 			<img src='src/assets/react-core-concepts.png' alt='Stylized atom' />
 			<h1>React Essentials</h1>
-			<p>Fundamental React concepts you will need for almost any app you are going to build!</p>
+			<p>{description} React concepts you will need for almost any app you are going to build!</p>
 		</header>
+	)
+}
+
+// function CoreConcept(props) {
+// 	return (
+// 		<li>
+// 			<img src={props.image} alt={props.imageDesc} />
+// 			<h3>{props.title}</h3>
+// 			<p>{props.desc}</p>
+// 		</li>
+// 	)
+// }
+
+function CoreConcept({ image, title, description }) {
+	return (
+		<li>
+			<img src={image} alt={title} />
+			<h3>{title}</h3>
+			<p>{description}</p>
+		</li>
 	)
 }
 
@@ -13,7 +43,19 @@ function App() {
 		<div>
 			<Header />
 			<main>
-				<h2>Time to get started!</h2>
+				<section id='core-concepts'>
+					<h2>Core Concepts</h2>
+					<ul>
+						<CoreConcept
+							image={CORE_CONCEPTS[0].image}
+							title={CORE_CONCEPTS[0].title}
+							description={CORE_CONCEPTS[0].description}
+						/>
+						<CoreConcept {...CORE_CONCEPTS[1]} />
+						<CoreConcept {...CORE_CONCEPTS[2]} />
+						<CoreConcept {...CORE_CONCEPTS[3]} />
+					</ul>
+				</section>
 			</main>
 		</div>
 	)
